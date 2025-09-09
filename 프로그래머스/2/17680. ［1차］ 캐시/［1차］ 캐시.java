4ec20809c7b9d10f64a80d3cf994aae92ccb6544
EUNJI LEE <dmswldk28@gmail.com>
2025-09-09 20:51:18 +0900

    [level 2] Title: [1차] 캐시, Time: 28.07 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,30 +1,33 @@
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        
-        if(cacheSize == 0){
+
+        if (cacheSize == 0) {
             return 5 * cities.length;
         }
-        
-        // 오래될수록 배열 번호 1, 2, 3,,,
-        List<String> list = new LinkedList<>();
-        
-        for(String city : cities){
+
+        Map<String, String> cache = new LinkedHashMap<String, String>(cacheSize, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > cacheSize;
+            }
+        };
+
+        for (String city : cities) {
             city = city.toUpperCase();
-            
-            if (list.contains(city)) {
-                list.remove(city);
+
+            // cache hit
+            if (cache.containsKey(city)) {
+                cache.get(city);
                 answer++;
-            } else {
-                // 캐시 미스
-                if (list.size() == cacheSize)  list.remove(0);
+            } else { // cache miss
+                cache.put(city, city);
                 answer += 5;
             }
-            list.add(city);
         }
-        
         return answer;
     }
 }
