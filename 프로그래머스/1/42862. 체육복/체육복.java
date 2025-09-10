7e49626b915e38072099bd66125a67857e00d364
EUNJI LEE <dmswldk28@gmail.com>
2025-09-10 23:51:59 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,10 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int answer = n;
         
         int[] arr = new int[n+1];
-        Arrays.fill(arr, 1);
         
         for(int r : reserve){
             arr[r]++;
@@ -16,19 +15,17 @@ class Solution {
         }
         
         for(int l=1; l<=n; l++){
-            if(arr[l] == 0){
-                if(arr[l-1] == 2){
+            if(arr[l] == -1){
+                if(arr[l-1] == 1){
                     arr[l-1]--;
                     arr[l]++;
-                }else if(l+1<=n && arr[l+1] == 2){
+                }else if(l+1<=n && arr[l+1] == 1){
                     arr[l+1]--;
                     arr[l]++;
+                }else{
+                    answer--;
                 }
             }
-        }
-        
-        for(int i=1; i<=n; i++){
-            if(arr[i] > 0) answer++;
         }
         
         return answer;
