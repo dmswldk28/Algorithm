5b022993df9d9f6f80f9e3915dba52716f47439c
EUNJI LEE <dmswldk28@gmail.com>
2025-09-19 18:31:43 +0900

    [level 2] Title: [3차] n진수 게임, Time: 9.59 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,21 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder gameSequence = new StringBuilder();
+
+        int requiredNumbers = t * m;
+        for (int i = 0; gameSequence.length() < requiredNumbers; i++) {
+            gameSequence.append(Integer.toString(i, n));
+        }
+
+        for (int i = 0; i < t; i++) {
+            int tubeIndex = (i * m) + (p - 1);
+            char tubeTurnNumber = gameSequence.charAt(tubeIndex);
+            answer.append(Character.toUpperCase(tubeTurnNumber));
+        }
+
+        return answer.toString();
+    }
+}
