4a7f20a22900b639d693c494d0c860c9ac75fc3b
EUNJI LEE <dmswldk28@gmail.com>
2025-09-25 19:37:15 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 33.86 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,27 +1,88 @@
-import java.util.*;
-import java.util.regex.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
-    public String[] solution(String[] files) {
-    Pattern p = Pattern.compile("([a-z\\s.-]+)([0-9]{1,5})");
 
-    Arrays.sort(files, new Comparator<String>() {
-        @Override
-        public int compare(String s1, String s2) {
-            Matcher m1 = p.matcher(s1.toLowerCase());
-            Matcher m2 = p.matcher(s2.toLowerCase());
-            m1.find();
-            m2.find();
-
-            if(!m1.group(1).equals(m2.group(1))) {
-                return m1.group(1).compareTo(m2.group(1));
-            } else {
-                return Integer.parseInt(m1.group(2)) - Integer.parseInt(m2.group(2));
+    private String[] splitFileName(String fileName) {
+        String head = "";
+        String number = "";
+        String tail = "";
+        
+        int n = fileName.length();
+        int headEnd = -1;
+        
+        // 1. HEAD 찾기: 숫자가 아닌 문자열의 끝
+        for (int i = 0; i < n; i++) {
+            if (Character.isDigit(fileName.charAt(i))) {
+                headEnd = i;
+                break;
             }
         }
-    });
+        
+        if (headEnd != -1) {
+            head = fileName.substring(0, headEnd);
+        } else {
+            return new String[]{fileName, "", ""}; 
+        }
 
-    return files;
+        int numberEnd = headEnd;
+        // 2. NUMBER 찾기: 첫 번째 숫자부터 시작하여 연속된 숫자, NUMBER는 최대 5글자
+        for (int i = headEnd; i < n; i++) {
+            if (Character.isDigit(fileName.charAt(i)) && (i - headEnd < 5)) {
+                numberEnd = i + 1;
+            } else {
+                break;
+            }
+        }
+        
+        // NUMBER: HEAD 끝부터 NUMBER 끝까지
+        if (numberEnd > headEnd) {
+            number = fileName.substring(headEnd, numberEnd);
+        }
+        
+        // 3. TAIL: NUMBER 끝부터 파일명 끝까지
+        if (numberEnd < n) {
+            tail = fileName.substring(numberEnd);
+        }
+        
+        return new String[]{head, number, tail};
+    }
 
+    public String[] solution(String[] files) {
+        Arrays.sort(files, new Comparator<String>() {
+            @Override
+            public int compare(String file1, String file2) {
+                String[] parts1 = splitFileName(file1);
+                String[] parts2 = splitFileName(file2);
+
+                String head1 = parts1[0];
+                String number1 = parts1[1];
+                
+                String head2 = parts2[0];
+                String number2 = parts2[1];
+                
+                // 1. HEAD 부분 비교 (대소문자 무시)
+                int headCompare = head1.toLowerCase().compareTo(head2.toLowerCase());
+                
+                if (headCompare != 0) {
+                    return headCompare;
+                }
+                
+                // 2. HEAD가 같으면 NUMBER 부분 비교 (숫자 값 비교)
+                int num1 = Integer.parseInt(number1);
+                int num2 = Integer.parseInt(number2);
+                
+                int numberCompare = num1 - num2;
+                
+                if (numberCompare != 0) {
+                    return numberCompare;
+                }
+                
+                // 3. HEAD와 NUMBER가 모두 같으면 원래 순서를 유지 (안정 정렬을 위해 0 반환)
+                return 0;
+            }
+        });
+
+        return files;
     }
 }
