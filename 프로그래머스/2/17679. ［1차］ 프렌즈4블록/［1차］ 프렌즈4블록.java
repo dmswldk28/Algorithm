7c9d51ea3a555cf831c74e1e536dabd38d921403
EUNJI LEE <dmswldk28@gmail.com>
2025-09-29 01:00:01 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 13.47 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -2,65 +2,63 @@ import java.util.*;
 
 class Solution {
     public int solution(int m, int n, String[] board) {
-        char[][] grid = new char[m][n];
-        for (int i = 0; i < m; i++) {
-            grid[i] = board[i].toCharArray();
-        }
-
         int answer = 0;
         
-        while (true) {
+        char[][] grid = new char[m][n];
+        for(int i=0; i<m; i++){
+            grid[i] = board[i].toCharArray();
+        }
+        
+        while(true){
             boolean[][] toRemove = new boolean[m][n];
-            int removedCount = 0;
-
-            for (int r = 0; r < m - 1; r++) {
-                for (int c = 0; c < n - 1; c++) {
-                    char block = grid[r][c];
+            int cnt = 0;
+            
+            for(int i=0; i<m-1; i++){
+                for(int j=0; j<n-1; j++){
+                    char block = grid[i][j];
                     
-                    if (block == '0') continue; 
+                    if(block == '0') continue;
                     
-                    if (block == grid[r][c+1] && 
-                        block == grid[r+1][c] && 
-                        block == grid[r+1][c+1]) {
-                        
-                        toRemove[r][c] = true;
-                        toRemove[r][c+1] = true;
-                        toRemove[r+1][c] = true;
-                        toRemove[r+1][c+1] = true;
+                    if(block == grid[i][j+1] &&
+                       block == grid[i+1][j] &&
+                       block == grid[i+1][j+1]){
+                        toRemove[i][j] = true;
+                        toRemove[i][j+1] = true;
+                        toRemove[i+1][j] = true;
+                        toRemove[i+1][j+1] = true;
                     }
                 }
             }
-
-            for (int r = 0; r < m; r++) {
-                for (int c = 0; c < n; c++) {
-                    if (toRemove[r][c]) {
-                        if (grid[r][c] != '0') {
-                            removedCount++;
-                            grid[r][c] = '0';
+            
+            for(int i=0; i<m; i++){
+                for(int j=0; j<n; j++){
+                    if(toRemove[i][j]){
+                        if(grid[i][j] != '0'){
+                            cnt++;
+                            grid[i][j] = '0';
                         }
                     }
                 }
             }
             
-            if (removedCount == 0) {
-                break;
-            }
+            if(cnt == 0) break;
             
-            answer += removedCount;
-
-            for (int c = 0; c < n; c++) {
-                for (int r = m - 1; r >= 0; r--) {
-                    if (grid[r][c] == '0') {
-                        for (int k = r - 1; k >= 0; k--) {
-                            if (grid[k][c] != '0') {
-                                grid[r][c] = grid[k][c];
-                                grid[k][c] = '0';
+            answer += cnt;
+            
+            for(int j=0; j<m; j++){
+                for(int i=m-1; i>=0; i--){
+                    if(grid[i][j] == '0'){
+                        for(int k = i-1; k>=0; k--){
+                            if(grid[k][j] != '0'){
+                                grid[i][j] = grid[k][j];
+                                grid[k][j] = '0';
                                 break;
                             }
                         }
                     }
                 }
             }
+            
         }
 
         return answer;
